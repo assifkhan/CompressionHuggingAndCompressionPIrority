6641cb95e11345a2b3b090b7dc6df51412cc77ec
asif <assifkhan@gmail.com>
2021-02-21 14:17:26 -0600

    Initial Commit

---
+++ CompressionHuggingAndCompressionPIrority/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CompressionHuggingAndCompressionPIrority
+//
+//  Created by Asif Khan on 2/21/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CompressionHuggingAndCompressionPIrority/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CompressionHuggingAndCompressionPIrority
+//
+//  Created by Asif Khan on 2/21/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
